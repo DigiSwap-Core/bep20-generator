5a096db945804f3d7caf6e9fbfa601b61cb5eef7
tokencenter <80170039+tokencenter@users.noreply.github.com>
2022-06-24 15:35:53 +0200

    Update tokenDetails.js

--- .vuepress/mixins/tokenDetails.js
+++ .vuepress/mixins/tokenDetails.js
@@ -35,7 +35,7 @@ export default {
           tokenRecover: false,
           removeCopyright: false,
           originalPrice: 0.5,
-          price: 0.15,
+          price: 0.1,
           gas: 1333276,
         },
         {
@@ -53,7 +53,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 0.75,
-          price: 0.25,
+          price: 0.15,
           gas: 1260139,
         },
         {
@@ -71,7 +71,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1,
-          price: 0.3,
+          price: 0.2,
           gas: 1444650,
         },
         {
@@ -89,7 +89,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1,
-          price: 0.3,
+          price: 0.2,
           gas: 1524151,
         },
         {
@@ -107,7 +107,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1.25,
-          price: 0.35,
+          price: 0.25,
           gas: 1708581,
         },
         {
@@ -125,7 +125,7 @@ export default {
           tokenRecover: false,
           removeCopyright: true,
           originalPrice: 1.5,
-          price: 0.45,
+          price: 0.3,
           gas: 1637163,
         },
         {
@@ -143,7 +143,7 @@ export default {
           tokenRecover: true,
           removeCopyright: true,
           originalPrice: 2,
-          price: 0.5,
+          price: 0.4,
           gas: 2254686,
         },
       ],
