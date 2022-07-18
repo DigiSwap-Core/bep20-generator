9db3a6a6ec0197b1887defad1915df223fab683a
Marquis Ogre <marquisogre@gmail.com>
2022-07-19 00:10:35 +0530

    Update Faq.vue

--- .vuepress/components/ui/Faq.vue
+++ .vuepress/components/ui/Faq.vue
@@ -201,7 +201,7 @@
                                         This behaviour is derived by ERC20 Token Recover.
                                     </span>
                                 </b-card-text>
-                                <b-link href="https://tokencenter.github.io/eth-token-recover/"
+                                <b-link href="https://digiswap-core.github.io/eth-token-recover/"
                                         class="card-link" target="_blank">
                                     Discover Token Recover
                                 </b-link>
